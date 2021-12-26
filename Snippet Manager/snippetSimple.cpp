a3400b688261dc0df3086ba796ba44365abea36d
saitejsunkara9 <saitejsunkara9@gmail.com>
2021-12-26 15:54:55 +0530

    Snippet Updated V1.2

--- Snippet Manager/snippetSimple.cpp
+++ Snippet Manager/snippetSimple.cpp
@@ -12,6 +12,7 @@ void solve(int test) {
 }
 
 int32_t main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     clock_t initialTime = clock();
     #ifndef ONLINE_JUDGE
         freopen("input.txt", "r", stdin);
