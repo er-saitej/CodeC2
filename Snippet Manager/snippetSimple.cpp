670160f4b304041829aa658cbf1ac3c86790ce6b
saitejsunkara9 <saitejsunkara9@gmail.com>
2021-12-26 15:20:48 +0530

    Snippet updated

--- Snippet Manager/snippetSimple.cpp
+++ Snippet Manager/snippetSimple.cpp
@@ -1,15 +1,19 @@
 #include<bits/stdc++.h>
+#include<algorithm>
+#include<vector>
+#include<stack>
+#include<queue>
+#define int long long int;
 using namespace std;
 
-int main() {
+int32_t main() {
+    clock_t initialTime = clock();
     #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
+        freopen("input", "r", stdin);
         freopen("output.txt", "w", stdout);
         freopen("error.txt", "w", stderr);
     #endif
-    int n;
-    cin>>n;
-    cout<<n;
-    cerr<<n;
+
+    cerr<< ((double)clock() - initialTime)/CLOCKS_PER_SEC;
     return 0;
 }
