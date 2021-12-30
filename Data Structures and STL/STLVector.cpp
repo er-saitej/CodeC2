01a9d3f60bd9b7f13225a0f65b4a9c6e124bb8a6
saitejsunkara9 <saitejsunkara9@gmail.com>
2021-12-30 10:56:40 +0530

    STL Vector implemented

---
+++ Data Structures and STL/STLVector.cpp
@@ -0,0 +1,62 @@
+/*
+    author: Sai Tej Sunkara
+    language: C++ Advanced (clang++ 13.x)
+*/
+#include<bits/stdc++.h>
+#define int long long int
+#define endl "\n"
+using namespace std;
+
+void displayVector(vector<int> &vector) {
+    cout<< "Displaying vector"<<endl;
+    for(auto x: vector) {
+        cout<< x<< " ";
+    }
+    cout<<endl;
+}
+
+void popAndClearVector(vector<int> &vector) {
+    cout<< "Show and Pop"<< endl;
+    for(auto x: vector) {
+        cout<< vector.back()<< " ";
+        vector.pop_back();
+    }
+    cout<<endl;
+}
+
+void solve() {
+    vector<int> vector(4, 20);
+    displayVector(vector);
+    for(int i=0;i<6;i++) {
+        int value;
+        cin>>value;
+        vector.push_back(value);
+    }
+    displayVector(vector);
+    cout<< "Size and capacity: "<<vector.size()<< " "<< vector.capacity()<< endl;
+    vector.push_back(20);
+    vector.push_back(15);
+    vector.push_back(2);
+    displayVector(vector);
+    cout<< "Size and capacity: "<<vector.size()<< " "<< vector.capacity()<< endl;
+    sort(vector.begin(), vector.end());
+    displayVector(vector);
+    popAndClearVector(vector);
+    displayVector(vector);
+    cout<< "Size and capacity: "<<vector.size()<< " "<< vector.capacity()<< endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    clock_t initialTime = clock();
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("error.txt", "w", stderr);
+        freopen("output.txt", "w", stdout);
+    #endif
+    int test;
+    cin>>test;
+    while(test--) solve();
+    cerr<< ((double)clock() - initialTime)/CLOCKS_PER_SEC;
+    return 0;
+}
